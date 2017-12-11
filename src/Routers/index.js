0239f0a754cdf245a50c0541a207c3e464e651fb
tarik sahni <tarik_sahni@external.mckinsey.com>
2017-12-11 17:50:11 +0530

    Login Added

--- src/Routers/index.js
+++ src/Routers/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Scene, Router } from 'react-native-router-flux';
-// import LoginForm from '../Components/LoginForm';
+import LoginForm from '../Components/LoginForm';
 import Messages from '../Components/Messages';
 import SwiperImages from '../Components/SwiperImages';
 import DashBoard from '../Components/DashBoard';
@@ -10,7 +10,7 @@ import { styles } from './styles';
 const RouterComponent = () => {
   return (
     <Router sceneStyle={{ paddingTop: 10 }}>
-      {/* <Scene key="auth" component={LoginForm} hideNavBar /> */}
+      <Scene key="auth" component={LoginForm} hideNavBar />
       <Scene key="main">
         <Scene
           key="dashboard"
