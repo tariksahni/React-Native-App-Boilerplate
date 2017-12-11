aa31f1591f9b75ef63fb59fa9d9ebbb53a858541
tarik sahni <tarik_sahni@external.mckinsey.com>
2017-12-11 17:47:02 +0530

    NavBar Styling

--- src/Routers/index.js
+++ src/Routers/index.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import { Scene, Router } from 'react-native-router-flux';
-import LoginForm from '../Components/LoginForm';
+// import LoginForm from '../Components/LoginForm';
 import Messages from '../Components/Messages';
 import SwiperImages from '../Components/SwiperImages';
 import DashBoard from '../Components/DashBoard';
+import homeImage from '../assets/Images/home.png';
 import { styles } from './styles';
 
 const RouterComponent = () => {
   return (
     <Router sceneStyle={{ paddingTop: 10 }}>
-      <Scene key="auth" component={LoginForm} hideNavBar />
+      {/* <Scene key="auth" component={LoginForm} hideNavBar /> */}
       <Scene key="main">
         <Scene
           key="dashboard"
@@ -20,13 +21,24 @@ const RouterComponent = () => {
         />
         <Scene
           key="messages"
-          hideBackImage
           component={Messages}
           title="Messages"
+          backButtonImage={homeImage}
+          leftButtonStyle={styles.leftButtonStyle}
+          leftButtonIconStyle={styles.leftButtonIconStyle}
           navigationBarStyle={styles.navigationBarStyle}
           titleStyle={styles.titleStyle}
         />
-        <Scene key="swipper" component={SwiperImages} hideNavBar />
+        <Scene
+          key="swipper"
+          component={SwiperImages}
+          title="Memories"
+          backButtonImage={homeImage}
+          leftButtonStyle={styles.leftButtonStyle}
+          leftButtonIconStyle={styles.leftButtonIconStyle}
+          navigationBarStyle={styles.navigationBarStyle}
+          titleStyle={styles.titleStyle}
+        />
       </Scene>
     </Router>
   );
--- src/Routers/styles.js
+++ src/Routers/styles.js
@@ -2,13 +2,20 @@ import { StyleSheet } from 'react-native';
 
 export const styles = StyleSheet.create({
   navigationBarStyle: {
-    backgroundColor: '#30625a',
+    backgroundColor: '#284C3F',
     height: '12%'
   },
   titleStyle: {
     fontFamily: 'HelveticaNeueMedium',
-    // color: '#d3d3d3',
     color: 'white',
     marginTop: 19
+  },
+  leftButtonStyle: {
+    marginTop: 19,
+    marginLeft: 13
+  },
+  leftButtonIconStyle: {
+    width: 20,
+    height: 20
   }
 });
